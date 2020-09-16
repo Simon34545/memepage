8851cafad4a95d96fdd9145ac1ba948cbbdf2359
Simon34545 <70719312+Simon34545@users.noreply.github.com>
2020-09-16 11:29:59 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
-self.addEventListener('push', function(event) {
+function send(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
   const title = 'Push Codelab';
   const options = {
-    body: 'Yay it works.',
+    body: 'NEVER GONNA GIVE YOU UP.',
     icon: 'memepage/favicon.ico',
     badge: 'memepage/favicon.ico'
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
+}
+
+
+self.addEventListener('push', send(event);
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    send();
+  );
 });
