2d421a367f685862d76155dbbb7033df03ac9624
Simon34545 <70719312+Simon34545@users.noreply.github.com>
2020-09-16 13:18:29 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
-function send() {
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
   const title = 'Push Codelab';
   const options = {
-    body: 'NEVER GONNA GIVE YOU UP.',
+    body: 'nEvEr gonna givE you up',
     icon: 'memepage/favicon.ico',
     badge: 'memepage/favicon.ico'
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
-}
+});
 
-
-self.addEventListener('push', send());
 self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
 
   event.notification.close();
 
   event.waitUntil(
-    send();
+    clients.openWindow('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
   );
 });
